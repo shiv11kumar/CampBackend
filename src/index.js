bb825ff126a551edb32c2bc905a4d7056de45f9e
Shiv Kumar <merai11shiv@gmail.com>
2026-06-24 17:46:11 +0530

    Mongo Db integration

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/index.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-
 const port = process.env.port || 3000;
 
-
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running at http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB Connection error", error);
+    process.exit(1);
+  });
